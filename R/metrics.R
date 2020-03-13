dec442cd318abfb273c187491a4900710b1bf6a0
schuetzingit <justinschuetz@gmail.com>
2020-03-13 12:16:25 -0400

    corrected metrics_pull, expanded description of parameters and values, wordsmithed, removed r_mean from metrics_distill stack

--- R/metrics.R
+++ R/metrics.R
@@ -1,11 +1,17 @@
-#' Transform a raster stack to intensity values
+#' Transform raster stack values to intensity values
 #'
-#' @param x RasterStack with each layer typically corresponding to the
-#'   distribution of an individual or species at a given time interval.
+#' @param x RasterStack. Layers typically contain information about the distribution of
+#'   a single individual or species at multiple points in time.
+#'   Alternatively, layers may contain information about the distributions of
+#'   multiple individuals or species within a single time period. Other
+#'   conceptualizations are possible.
 #'
-#' @return A RasterStack resulting from converting the input stack to intensity
-#'   values by dividing each layer by the maximum pixel value in that layer.
-#'   These layer maxima are stored as the `"maximum"` attribute.
+#' @return A RasterStack containing intensity values.
+#'   Intensity values are calculated by dividing cell values in every layer
+#'   by the maximum cell value in the entire stack, thus ensuring intensities
+#'   are comparable across layers.
+#'
+#'   The maximum cell value in the stack is stored as the `"maximum"` attribute.
 #'
 #' @family metrics
 #' @export
@@ -14,42 +20,57 @@
 #' data("elephant_ud")
 #' r <- metrics_pull(elephant_ud)
 #' print(r)
-#' # maximum values for each layer stored as an attribute
+#' # maximum value for the stack stored as an attribute
 #' attr(r, "maximum")
 metrics_pull <- function(x) {
   stopifnot(class(x) %in% c("RasterStack", "RasterBrick"))
 
-  # scale layers by their maximum
+  # scale layers by the maximum value in the stack
   maximum <- raster::cellStats(x, "max")
   if (any(is.na(maximum)) && !all(is.finite(maximum))) {
     stop("All layers must have at least one non-NA value.")
   }
-  r <- x / maximum
+  r <- x / max(maximum)
 
   # set attributes
   attr(r, "metric") <- "pull"
-  attr(r, "maximum") <- maximum
+  attr(r, "maximum") <- max(maximum)
 
   return(r)
 }
 
-#' Distill a raster stack into a set of metrics for each cells
+#' Distill a raster stack into a set of distribution metrics in each cell
 #'
-#' @param x RasterStack with each layer typically corresponding to the
-#'   distribution of an individual or species at a given time interval.
+#' @param x RasterStack. Layers typically contain information about the distribution of
+#'   a single individual or species at multiple points in time.
+#'   Alternatively, layers may contain information about the distributions of
+#'   multiple individuals or species within a single time period. Other
+#'   conceptualizations are possible.
 #'
 #' @return A RasterStack with four layers:
-#'   - `intensity`: the maximum pixel intensity across all layers
-#'   - `intensity_mean`: the mean pixel intensity across all layers
-#'   - `layer`: the layer number associated with the maximum intensity.
-#'   - `specificity`: the amount of variation between layers for each cell, this
-#'   is a measure of seasonality if the layers represents different times of
-#'   year.
-#'   - `n_layers`: the number of intervals with a non-NA pixel value
+#'   - `intensity`: the maximum intensity value across all layers.
+#'   - `layer`: the layer containing the maximum intensity value.
+#'   - `specificity`: the degree to which intensity values are unevenly distributed across layers (see Details).
+#'   - `n_layers`: the number of layers with non-NA values (see Details).
 #'
-#'   Here intensity is the result of dividing all cell values by the maximum
-#'   cell value across all layers. This maximum value is stored as the
-#'   `"maximum"` attribute.
+#'   The maximum cell value in the stack is stored as the `"maximum"` attribute.
+#'
+#' @details Specificity values range from 0 to 100. Values of 0 indicate
+#'   intensity values are identical in all layers. Values of 100 indicate
+#'   intensity values are restricted to a single layer. Interpretation of specificity values depends on the layers provided.
+#'   If layers describe the distribution of a species at different times of
+#'   the year, specificity can be interpreted as a measure of seasonality
+#'   (i.e., 0 = stable year-round occurrence in a cell, 100 = highly seasonal occurrence).
+#'   If layers describe space use by multiple individuals, specificity
+#'   can be interpreted as a measure of exclusivity (i.e., 0 = equal use of a cell
+#'   by all individuals, 100 = exclusive use by one individual).
+#'
+#'   The number of layers with non-NA values is recorded to aid interpretation of
+#'   distributions. Ideally, users have equal knowledge of distributions in each layer
+#'   and n_layers values are identical in every cell. Distributions are more likely
+#'   to be misrepresented and misinterpreted if cells do not contain intensity values
+#'   in every layer.
+#'
 #'
 #' @family metrics
 #' @export
@@ -64,7 +85,7 @@ metrics_distill <- function(x) {
   stopifnot(class(x) %in% c("RasterStack", "RasterBrick"))
 
   # scale layers by overall maximum
-  maximum <- raster::cellStats(x, "max")
+  maximum <- raster::cellStats(x, "max", na.rm = TRUE)
   if (any(is.na(maximum)) && !all(is.finite(maximum))) {
     stop("All layers must have at least one non-NA value.")
   }
@@ -84,9 +105,8 @@ metrics_distill <- function(x) {
     r_peak <- raster::calc(intensity, which_max)
     r_specificity <- raster::calc(intensity, specificity)
   })
-  r <- raster::stack(r_max, r_mean, r_peak, r_specificity, r_n)
-  names(r) <- c("intensity", "intensity_mean",
-                "layer", "specificity", "n_layers")
+  r <- raster::stack(r_max, r_peak, r_specificity, r_n)
+  names(r) <- c("intensity", "layer", "specificity", "n_layers")
 
   # set attributes
   attr(r, "metric") <- "distill"
