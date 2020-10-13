073a0adc8a7f58478a2f7f6c71e2f4176e977d05
Matt Strimas-Mackey <mstrimas@gmail.com>
2020-10-13 08:12:48 -0700

    matt's updates

--- data-raw/rensselaer-buildings.R
+++ data-raw/rensselaer-buildings.R
@@ -5,4 +5,3 @@ buildings <- read_sf("data-raw/rensselaer-buildings.gpkg") %>%
   st_transform(crs = st_crs(fisher_ud)) %>%
   st_crop(st_bbox(fisher_ud))
 saveRDS(buildings, "data-raw/buildings.rds")
-usethis::use_data(buildings, overwrite = TRUE)
